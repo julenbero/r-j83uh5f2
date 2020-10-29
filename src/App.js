dfb1400ffde4e2c06620219e2f380ae4d941d5af
julenbero <julenbero@gmail.com>
2020-10-29 17:55:27 -0500

    React Count

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
+	state = { counter: 0 };
+
+	changeText(event) {
+		const { textLength } = event.target;
+		this.setState({ counter: textLength });
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<textarea rows="3" onChange={(event) => this.changeText(event)} />
+				<div className="counter">{this.state.counter}</div>
+			</div>
+		);
+	}
 }
 
 export default App;
